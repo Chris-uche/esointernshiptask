857019920371f459d989da680568d6f731515d20
Chris-uche <Christogonus2010@yahoo.com>
2021-04-01 04:00:06 +0100

    The src directory

---
+++ esointern/src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+
+
+import React from "react"
+import endpoint from './endpoint';
+import {useEffect,useState} from "react";
+
+
+
+  function App() {
+
+    const [data, setData] = useState([]);
+    const [loading,setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    
+      useEffect(()=>{
+        setData([])
+        setLoading(true)
+        setError(null)
+
+        fetch(endpoint)
+        .then((res)=>res.json())
+        .then(response=>{
+          setLoading(false)
+          //I had to desctructure the response here and return only the store values, the values are raw values not formatted
+          const dataStrings = response.data.map(({store})=> store)
+          setData(dataStrings)
+        })
+        .catch(error=>{
+          setLoading(false)
+          setError(error)
+
+        })
+       
+        
+      },[])
+
+
+      return (
+        <div className="App">
+        <header>
+        <h1>Data Display</h1>
+        {
+          loading ? (<p>Loading.....</p>) : (
+            <p>{data}</p>
+          )
+        }
+        {error && <p>{error.message}</p>}
+        </header>
+      </div>
+      );
+    }
+
+export default App;
+    
+    
+
